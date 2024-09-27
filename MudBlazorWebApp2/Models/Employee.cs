543d636574f1d6a0de4dbb6050bb6700806e20e1
megriep <markgriep@users.noreply.github.com>
2024-09-27 15:58:14 -0500

    Add project files.

---
+++ MudBlazorWebApp2/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+
+
+namespace MudBlazorWebApp2.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string EmployeeId { get; set; }
+        public string Department { get; set; }
+    }
+}
