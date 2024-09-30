d9ac229e9b647bf5e0dff559c8ea3d6d9697b550
megriep <markgriep@users.noreply.github.com>
2024-09-30 13:41:41 -0500

    waypoint

---
+++ MudBlazorWebApp2/Models/SuperEmployee.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+
+
+namespace MudBlazorWebApp2.Models
+{
+    public class SuperEmployee
+    {
+        public string? EmployeeName { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime EmployeeDepartment { get; set; }
+    }
+}
