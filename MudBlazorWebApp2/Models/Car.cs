52f106c70a8cbff2f4d1610948074d6980cbb689
megriep <markgriep@users.noreply.github.com>
2024-10-01 11:34:20 -0500

    added another dialog.  This time with validation.

---
+++ MudBlazorWebApp2/Models/Car.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MudBlazorWebApp2.Models
+{
+    public class Car
+    {
+        [DisplayName("THE OWNER")]
+        [Required(ErrorMessage = "Owner name is required.")]
+        public string? OwnerName { get; set; }
+
+        [Required(ErrorMessage = "Make is required.")]
+        public string Make { get; set; }
+
+        [Required(ErrorMessage = "Model is required.")]
+        public string Model { get; set; }
+
+        [Required(ErrorMessage = "Year is required.")]
+        [Range(1886, 2024, ErrorMessage = "Year must be between 1886 and 2055.")]
+        public int Year { get; set; }
+
+        [StringLength(10, ErrorMessage = "Description can't exceed 10 characters.")]
+        public string Description { get; set; }
+
+    }
+}
