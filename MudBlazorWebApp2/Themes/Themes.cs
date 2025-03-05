02b9b90acd50f0995fba04ae289abc98b59fad07
megriep <markgriep@users.noreply.github.com>
2025-03-05 14:56:45 -0600

    Worked with themes put back the original colors.

--- MudBlazorWebApp2/Themes/Themes.cs
+++ MudBlazorWebApp2/Themes/Themes.cs
@@ -8,6 +8,53 @@ namespace MudBlazorDialogTest.Themes
     {
 
 
+        // the original light
+        public static PaletteLight _lightPalette = new()
+        {
+            Background = "#e3e2d0", // Set the background color here
+            Black = "#110e2d",
+            AppbarText = "#424242",
+            AppbarBackground = "rgba(255,255,255,0.8)",
+            DrawerBackground = "#ffffff",
+            GrayLight = "#e8e8e8",
+            GrayLighter = "#f9f9f9",
+        };
+
+
+        // this was the original dark
+        public static PaletteDark _darkPalette = new()
+        {
+            Primary = "#7e6fff",
+            Surface = "#1e1e2d",
+            Background = "#1a1a27",
+            BackgroundGray = "#151521",
+            AppbarText = "#92929f",
+            AppbarBackground = "rgba(26,26,39,0.8)",
+            DrawerBackground = "#1a1a27",
+            ActionDefault = "#74718e",
+            ActionDisabled = "#9999994d",
+            ActionDisabledBackground = "#605f6d4d",
+            TextPrimary = "#b2b0bf",
+            TextSecondary = "#92929f",
+            TextDisabled = "#ffffff33",
+            DrawerIcon = "#92929f",
+            DrawerText = "#92929f",
+            GrayLight = "#2a2833",
+            GrayLighter = "#1e1e2d",
+            Info = "#4a86ff",
+            Success = "#3dcb6c",
+            Warning = "#ffb545",
+            Error = "#ff3f5f",
+            LinesDefault = "#33323e",
+            TableLines = "#33323e",
+            Divider = "#292838",
+            OverlayLight = "#1e1e2d80",
+        };
+
+
+
+
+
         public static readonly PaletteLight LightPalette1 = new()
         {
             Black = "#000",
@@ -77,7 +124,7 @@ namespace MudBlazorDialogTest.Themes
             OverlayLight = "rgba(255,255,255,0.4980392156862745)",
         };
 
-        public static PaletteDark DarkPalette2 = new()
+        public static PaletteDark DarkPalette1 = new()
         {
             Black = "#000",
             White = "#fff",
@@ -206,10 +253,7 @@ namespace MudBlazorDialogTest.Themes
             OverlayLight = "rgba(255,255,255,0.4980392156862745)",
         };
 
-
-
-
-        public static PaletteDark PaletteDark2 = new()
+        public static PaletteDark DarkPalette2 = new()
         {
             Black = "#000",
             White = "#fff",
