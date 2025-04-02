3900e0a412ee600ef816dc3d84524ec1d179a13e
megriep <markgriep@users.noreply.github.com>
2025-04-02 13:50:32 -0500

    added selection percentages

---
+++ MudBlazorWebApp2/Models/DtoModel/DtoSelectionPercentage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace RandomDrugTest.Core.Models.DtoModels
+namespace MudBlazorDialogTest.Models.DtoModels
+{
+    public class DtoSelectionPercentage
+    {
+
+        public string? TestGroup { get; set; }
+
+        public string? TestType { get; set; }
+
+        public DateOnly UpdateDate { get; set; }
+
+        public int PercentageRequired { get; set; }
+
+    }
+}
---
+++ MudBlazorWebApp2/Models/SelectionPercentage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+//namespace RandomDrugTest.Core.Models
+namespace MudBlazorDialogTest.Models
+{
+
+    [Table("SelectionPercentages")]
+    public partial class SelectionPercentage
+    {
+        public int Id { get; set; }
+
+        public string? TestGroup { get; set; }
+
+        public string? TestType { get; set; }
+
+        //public string? UpdateDateString { get; set; }
+
+        public DateTime UpdateDate { get; set; }
+
+        public string? EffectiveStatus { get; set; }
+
+        public int PercentageRequired { get; set; }
+    }
+}
